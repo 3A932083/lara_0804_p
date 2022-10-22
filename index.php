428891a9f54c6e74b7b7d6220dfe08f9e39b2207
jaing shu yi <3a932083@gm.student.ncut.edu.tw>
2022-10-22 13:16:23 +0800

    使用Carbon函式修改日期時間

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ try {
     die();
 }
 
+//使用Carbon類別
+use Carbon\Carbon;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -82,7 +85,9 @@ try {
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">文章清單
-                    <small>{今日日期}</small>
+                    <small>
+                        <?php echo Carbon::now()->toDateString();?>
+                    </small>
                 </h1>
             </div>
         </div>
@@ -102,7 +107,7 @@ try {
                 <p><?=mb_substr($row->content, 0, 130, "utf-8"),'…'?></p>
                 <p class="text-right">
                     <span class="glyphicon glyphicon-time"></span>
-                    發表於 <?=$row->created_at?>
+                    發表於 <?=Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->diffForHumans()?>
                 </p>
             </div>
             <?php endwhile; ?>
